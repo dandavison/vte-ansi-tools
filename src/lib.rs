580fbb4ceb90e33342599e5676e98f90ae56bdc1
Dan Davison <dandavison7@gmail.com>
2022-09-16 09:04:58 -0400

    Expose the underlying iterator to users of the library

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,7 @@ use itertools::Itertools;
 use unicode_segmentation::UnicodeSegmentation;
 use unicode_width::UnicodeWidthStr;
 
-use iterator::{AnsiElementIterator, Element};
+pub use iterator::{AnsiElementIterator, Element};
 
 pub const ANSI_CSI_CLEAR_TO_EOL: &str = "\x1b[0K";
 pub const ANSI_CSI_CLEAR_TO_BOL: &str = "\x1b[1K";
